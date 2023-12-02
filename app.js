62c9aa5c95cd13cdd0ee12666ca26d0a8b016469
libp <924701358@qq.com>
2023-12-02 20:02:46 +0800

    pathRewrite 可以支持更多网站的反向代理请求

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const {
 const app = express()
 const port = 9000
 
-app.use('/', createProxyMiddleware({
+app.use('/openai', createProxyMiddleware({
   target: 'https://api.openai.com/',
   changeOrigin: true,
+  pathRewrite: {
+    '^/openai': '', 
+  },
   onProxyReq: (proxyReq, req, res) => {
     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
     proxyReq.removeHeader('x-forwarded-for');
@@ -21,31 +24,53 @@ app.use('/', createProxyMiddleware({
 
 
 
-// app.use('/google', createProxyMiddleware({
-//   target: 'https://google.com/',
-//   changeOrigin: true,
-//   onProxyReq: (proxyReq, req, res) => {
-//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-//     proxyReq.removeHeader('x-forwarded-for');
-//     proxyReq.removeHeader('x-real-ip');
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//   }
-// }));
+app.use('/google', createProxyMiddleware({
+  target: 'https://google.com/',
+  pathRewrite: {
+    '^/google': '', 
+  },
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
 
-// app.use('/github', createProxyMiddleware({
-//   target: 'https://github.com/',
-//   changeOrigin: true,
-//   onProxyReq: (proxyReq, req, res) => {
-//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-//     proxyReq.removeHeader('x-forwarded-for');
-//     proxyReq.removeHeader('x-real-ip');
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//   }
-// }));
+app.use('/github', createProxyMiddleware({
+  target: 'https://github.com/',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/github': '', 
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.use('/baidu', createProxyMiddleware({
+  target: 'https://baidu.com/',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/baidu': '', 
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
